d730608892868985dd6e0a6a6c32377506de72f6
Евгений <eplyago@mail.ru>
2022-11-08 22:24:43 +0300

    Lab 3 task 1

--- Lab3/Task3.py
+++ Lab3/Task3.py
@@ -1,1 +1,14 @@
-def Mean(x, y, AMean,GMean):
+import math
+def Mean(X, Y):
+    AMean = (X + Y) / 2
+    GMean = (math.sqrt(X * Y))
+    return AMean, GMean
+
+
+X = int(input("Enter X "))
+Y = int(input("Enter Y "))
+AMean, GMean = Mean(X, Y)
+print("Amean ", AMean)
+print("Gmean ", GMean)
+
+
